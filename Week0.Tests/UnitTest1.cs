98aef6111afb7bbf79f626c5e706737411908a04
Micolaj Sebastian Rudniak <miru@itu.dk>
2021-09-06 15:04:50 +0200

    Implemented

--- Week0.Tests/UnitTest1.cs
+++ Week0.Tests/UnitTest1.cs
@@ -1,40 +1,52 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Week0.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void test_404()
-        {
-            bool output = Program.IsLeapYear(404);
+        // Old tests
+        // [Fact]
+        // public void test_404()
+        // {
+        //     bool output = Program.IsLeapYear(404);
 
+        //     Assert.Equal(true, output);
+        // }
+
+        // [Fact]
+        // public void test_405()
+        // {
+        //     bool output = Program.IsLeapYear(405);
+
+        //     Assert.Equal(false, output);
+        // }
+
+        // [Fact]
+        // public void test_100()
+        // {
+        //     bool output = Program.IsLeapYear(100);
+
+        //     Assert.Equal(false, output);
+        // }
+
+        [Fact]
+        public void test_working_number()
+        {
+
+            bool output = Program.IsLeapYear(2000);
+            
             Assert.Equal(true, output);
         }
 
         [Fact]
-        public void test_405()
+        public void test_false_big_number()
         {
-            bool output = Program.IsLeapYear(405);
 
+            bool output = Program.IsLeapYear(1999);
+            
             Assert.Equal(false, output);
-        }
-
-        [Fact]
-        public void test_100()
-        {
-            bool output = Program.IsLeapYear(100);
-
-            Assert.Equal(false, output);
-        }
-
-        [Fact]
-        public void test_400()
-        {
-            bool output = Program.IsLeapYear(400);
-
-            Assert.Equal(true, output);
         }
     }
 }
--- Week0/Program.cs
+++ Week0/Program.cs
@@ -6,25 +6,46 @@ namespace Week0
     {
         static void Main(string[] args)
         {
-            int input = Int32.Parse(Console.ReadLine());    
-            PrintYayOrNay(IsLeapYear(input));
+            int input;
+            try
+            {
+                input = Int32.Parse(Console.ReadLine()); 
+                PrintYayOrNay(IsLeapYear(input));
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Plz enter a simple integer :)");
+                Main(new string[0]);
+            }
+            
         }
 
 
-
-
-        public static bool IsLeapYear(int year){
-           return (year%4==0 && (year%100!=0 || year%400==0));
+        public static bool IsLeapYear(int year)
+        {
+            if (year > 1581)
+            {
+                return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+            }
+            else
+            {
+                Console
+                    .WriteLine("This program only handles years after 1581, try again");
+                int input = Int32.Parse(Console.ReadLine());
+                return IsLeapYear(input);
+            }
         }
 
-        public static void PrintYayOrNay(bool IsLeapYear){
-           if(IsLeapYear){
-              Console.WriteLine("yay"); 
-           } else {
-               Console.WriteLine("nay"); 
-           }
+        public static void PrintYayOrNay(bool IsLeapYear)
+        {
+            if (IsLeapYear)
+            {
+                Console.WriteLine("yay");
+            }
+            else
+            {
+                Console.WriteLine("nay");
+            }
         }
-
-
     }
 }
