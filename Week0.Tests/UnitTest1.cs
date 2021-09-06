c2129f070acbb444831af8e63e7571ce9fda8ceb
Micolaj Sebastian Rudniak <miru@itu.dk>
2021-09-06 13:58:34 +0200

    Tested and Created

--- Week0.Tests/UnitTest1.cs
+++ Week0.Tests/UnitTest1.cs
@@ -6,9 +6,35 @@ namespace Week0.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void test_404()
         {
+            bool output = Program.IsLeapYear(404);
 
+            Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void test_405()
+        {
+            bool output = Program.IsLeapYear(405);
+
+            Assert.Equal(false, output);
+        }
+
+        [Fact]
+        public void test_100()
+        {
+            bool output = Program.IsLeapYear(100);
+
+            Assert.Equal(false, output);
+        }
+
+        [Fact]
+        public void test_400()
+        {
+            bool output = Program.IsLeapYear(400);
+
+            Assert.Equal(true, output);
         }
     }
 }
--- Week0/Program.cs
+++ Week0/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Week0
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int input = Int32.Parse(Console.ReadLine());    
+            PrintYayOrNay(IsLeapYear(input));
         }
+
+
+
+
+        public static bool IsLeapYear(int year){
+           return (year%4==0 && (year%100!=0 || year%400==0));
+        }
+
+        public static void PrintYayOrNay(bool IsLeapYear){
+           if(IsLeapYear){
+              Console.WriteLine("yay"); 
+           } else {
+               Console.WriteLine("nay"); 
+           }
+        }
+
+
     }
 }
